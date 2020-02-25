18cf4d3b0d3ef2c5da9fdadbb7518e7086306213
martengooz <martengooz@gmail.com>
2020-02-25 14:23:38 +0100

    Add documentation for functions

--- helpers.js
+++ helpers.js
@@ -1,27 +1,40 @@
 "use strict";
 
+/**
+ * Removes the # character from a string.
+ * @param {string} hashedString Any string.
+ */
 function dehash(hashedString) {
   return hashedString.replace("#", "");
 }
 
+/**
+ * Removes the @ character from a string.
+ * @param {string} taggedString Any string.
+ */
 function detag(taggedString) {
   return taggedString.replace("@", "");
 }
 
 /**
- * @param {string} message                          Original message that should be interpolated.
- * @param {Object.<string, string|number>} params   Replace key string with key value.
+ * Replaces every occurance of a template string expressions variable with provided template values.
+ * @param {string} message Original message with template string expressions.
+ * @param {{key: string, value: string|number}} replacements Replace every occurance of ${<key>} with <value> in <message>.
+ * @return {string} Replaced string.
  */
-function replaceInMessage(message, params) {
-  if (params) {
+function replaceInMessage(message, replacements) {
+  if (replacements) {
     var replacedString = message;
-    for (var key in params) {
-      if (!(key in params)) {
+    for (var key in replacements) {
+      if (!(key in replacements)) {
         continue;
       }
-      if (typeof params[key] === "string" || typeof params[key] === "number") {
+      if (
+        typeof replacements[key] === "string" ||
+        typeof replacements[key] === "number"
+      ) {
         var re = new RegExp(`\\$\\{${key}\\}`, "g");
-        replacedString = replacedString.replace(re, params[key]);
+        replacedString = replacedString.replace(re, replacements[key]);
       }
     }
     return replacedString;
