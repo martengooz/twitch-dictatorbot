bb15058884d42435a58a0cf7d354f6adefb236ef
martengooz <removed.due.to.reqruiter.spam@gooz.se>
2020-02-25 13:09:39 +0100

    Remove comments in fs mock

--- tests/__mocks__/fs.js
+++ tests/__mocks__/fs.js
@@ -2,9 +2,6 @@
 
 const fs = jest.genMockFromModule("fs");
 
-// This is a custom function that our tests can use during setup to specify
-// what the files on the "mock" filesystem should look like when any of the
-// `fs` APIs are used.
 let mockFiles = Object.create(null);
 function __setMockFiles(newMockFiles) {
   mockFiles = Object.create(null);
@@ -13,8 +10,6 @@ function __setMockFiles(newMockFiles) {
   }
 }
 
-// A custom version of `readdirSync` that reads from the special mocked out
-// file list set via __setMockFiles
 function existsSync(directoryPath) {
   return directoryPath in mockFiles;
 }
