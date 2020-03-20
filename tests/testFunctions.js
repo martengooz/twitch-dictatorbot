05e2cf7a779cde5951e974f9ad8a60001bdf1752
martengooz <removed.due.to.reqruiter.spam@gooz.se>
2020-03-20 09:14:49 +0100

    Use correct cfg path in tests

--- tests/testFunctions.js
+++ tests/testFunctions.js
@@ -1,7 +1,7 @@
 import path from "path";
 import * as cfg from "./cfg.json";
 
-const cfgPath = path.resolve("./cfg.json");
+const cfgPath = path.resolve("tests/cfg.json");
 const dbPath = path.resolve(cfg.dbPath);
 cfg.dbPath = dbPath;
 export { cfg, dbPath, cfgPath };
